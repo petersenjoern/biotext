e058304cd28cda34e7e65adb54da6e21cec9838c
petersenjoern <petersenjoern@protonmail.com>
2020-10-29 05:56:16 +0100

    notebook setup

--- biotext/ner.py
+++ biotext/ner.py
@@ -1,28 +1,75 @@
+#%%
 
+import torch
 from fastai.text.all import *
 from utils import TextDataLoadersInspector
 
 
-if __name__ == "__main__":
+#%%
+## Get the data for x
+path = untar_data(URLs.IMDB)
+files = get_text_files(path, folders=["train", "test"])
+files[0]
+
+#%%
+## Get data for y and convert y to idx (numericalize)
+lbls = files.map(Self.parent.name()).unique()
+v2i = lbls.val2idx()
+print(v2i)
+
+#%%
+
+# Build a Dataset for PyTorch (has to support indexing, e.g. __getitem__ and __len__)
+class Dataset:
+    """Dataset for Pytorch (has to support indexing of data, hence __getitem__ and __len__).
+    Input data is a list of file paths"""
     
-    path = untar_data(URLs.IMDB)
-    print(path.ls())
+    def __init__(self, fns):
+        self.fns = fns
+        
+    def __len__(self): 
+        return len(self.fns)
+    
+    def __getitem__(self, i):
+        txt = self.fns[i].open().read()
+        return txt
 
-    files = get_text_files(path, folders=["train"])
-    txts = L(o.open().read() for o in files[:2000])
-    print(txts[0])
+    
+def random_splitting(valid_pct=0.2, seed=None):
+    "Create function that splits items between train/val with valid_pct randomly."
+    def _inner(o):
+        if seed is not None: torch.manual_seed(seed)
+        rand_idx = L(int(i) for i in torch.randperm(len(o)))
+        cut = int(valid_pct * len(o))
+        return rand_idx[cut:],rand_idx[:cut]
+    return _inner
 
-    tok = Tokenizer.from_folder(path)
+
+#%%
+train, valid = random_splitting(valid_pct=0.1)(files) 
+train[0:5]
+
+
+#%%
+valid_ds = Dataset(valid)
+valid_ds[0]
+
+    # path = untar_data(URLs.IMDB)
+    # print(path.ls())
+
+    # files = get_text_files(path)
+    # txts = L(o.open().read() for o in files[:2000])
+    # print(txts[0])
+
+    # tok = Tokenizer.from_folder(path)
     # tok.setup(txts)
     # toks = txts.map(tok)
     # print(tok(txts[0]))
 
-    num = Numericalize
-
-
-    tfms = Pipeline([tok, num])
-    t = tfms(txts[0])
-    print(t)
+    # num = Numericalize
+    # tfms = Pipeline([tok, num])
+    # t = tfms(txts[0])
+    # print(t)
     
 
     # tls = TfmdLists(files, [Tokenizer.from_folder(path), Numericalize])
@@ -32,3 +79,4 @@ if __name__ == "__main__":
     # print(dls.show_batch())
     # learn = text_classifier_learner(dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
     # TextDataLoadersInspector(dls)
+#%%
